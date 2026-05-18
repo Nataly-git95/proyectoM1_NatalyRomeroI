01246d962708fe3ea1c9ca2a48e1deefc2ec8eeb
nataly romero i <anaroi030995@gmail.com>
2026-05-18 11:31:35 -0500

    feat:guardamos variables del html se activo la funcion del boton carga la cantidad de colores y formatos

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-function generateHex() {
+const cantidadcolores = document.getElementById("cantidad-colores");
+const formatocolores = document.getElementById("formatocolor");
+const crearboton = document.getElementById("crearboton");
+const contenedorpaleta = document.getElementById("contenedorpaleta");
+
+crearboton.addEventListener("click", generarpaleta);
+
+function generarpaleta(){
+
+    const cantidad = cantidadcolores.value;
+    const formato = formatocolores.value;
+
+    contenedorpaleta.innerHTML = "";
+
+    for(let i = 0; i < cantidad; i++){
+        let colorgenerado;
+        if(formato === "hex"){
+            colorgenerado = generarHex();
+            }else if(formato === "rgba"){
+                colorgenerado = generarRGBA();
+            }else{
+                colorgenerado = generarHSL();
+            }
+            creartarjetacolor(colorgenerado);
+    }
+    }
+
+function generarHex() {
     const letras = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
@@ -7,16 +34,28 @@ function generateHex() {
     return color
 }
 
-function generateRGBA() {
+function generarRGBA() {
     const r = Math.floor(Math.random() *256);
     const g = Math.floor(Math.random() *256);
     const b = Math.floor(Math.random() *256);
     return `rgba(${r}, ${g}, ${b}, 1)`;
 }
 
-function generateHSL() {
+function generarHSL() {
     const h = Math.floor(Math.random() *360);
     const s = Math.floor(Math.random() *100);
     const l = Math.floor(Math.random() *100);
     return `hsl(${h}, ${s}%, ${l}%)`;
 }
+
+function creartarjetacolor(color) {
+    const tarjeta = document.createElement("article");
+    tarjeta.classList.add("tarjeta-color");
+
+    const caja = document.createElement("div");
+    caja.style.backgroundColor = color;
+    caja.textContent = color;
+    tarjeta.appendChild(caja);
+    contenedorpaleta.appendChild(tarjeta);
+}
+generarpaleta();
